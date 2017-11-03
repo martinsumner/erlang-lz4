cf818c270b217b27801e4797d06a3c474a275ff9
Martin Sumner <martin.sumner@adaptip.co.uk>
2017-11-03 12:00:48 +0000

    Debugging

--- src/lz4.erl
+++ src/lz4.erl
@@ -29,6 +29,7 @@ init() ->
                   Path ->
                       Path
               end,
+    io:format("Loading NIF file from ~w~n", [PrivDir]),
     erlang:load_nif(filename:join(PrivDir, lz4), 0).
 
 %% @doc Equals `compress(Binary, [])'.
